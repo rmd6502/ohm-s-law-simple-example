b9d84f130a51c935feaa960a62e54888f2e85b66
Robert Diamond <rmd6502@gmail.com>
2011-12-18 19:17:27 -0500

    example for ios class

--- form_example/com_robertdiamondViewController.h
+++ form_example/com_robertdiamondViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface com_robertdiamondViewController : UIViewController
+@interface com_robertdiamondViewController : UIViewController<UITextFieldDelegate>
+
+@property (nonatomic,assign) IBOutlet UITextField *volts;
+@property (nonatomic,assign) IBOutlet UITextField *ohms;
+@property (nonatomic,assign) IBOutlet UITextField *amps;
+
+@property (nonatomic,assign) IBOutlet UIButton *resetButton;
+
+- (id)init;
+- (void)dealloc;
+
+- (IBAction) calculate:(id)sender;
+- (IBAction) reset:(id)sender;
+- (IBAction) checkResetButton:(id)sender;
+
+- (void)kbWillShow:(NSNotification *)notif;
+- (void)kbWillHide:(NSNotification *)notif;
 
 @end
--- form_example/com_robertdiamondViewController.m
+++ form_example/com_robertdiamondViewController.m
@@ -9,6 +9,22 @@
 #import "com_robertdiamondViewController.h"
 
 @implementation com_robertdiamondViewController
+@synthesize volts;
+@synthesize ohms;
+@synthesize amps;
+@synthesize resetButton;
+
+- (id)init {
+    self = [super init];
+    if (self != nil) {
+        // Do stuff here
+    }
+    return self;
+}
+- (void)dealloc {
+    // free stuff here
+    [super dealloc];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -21,7 +37,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
+    ((UIScrollView *)self.view).contentSize = self.view.bounds.size;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(kbWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(kbWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)kbWillShow:(NSNotification *)notif {
+    CGRect ht = [[[notif userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    
+    CGRect fr = self.view.frame;
+    fr.size.height -= ht.size.height;
+    self.view.frame = fr;
+    [self.view setNeedsLayout];
+}
+
+- (void)kbWillHide:(NSNotification *)notif {
+    CGRect ht = [[[notif userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    
+    CGRect fr = self.view.frame;
+    fr.size.height += ht.size.height;
+    self.view.frame = fr;
+    [self.view setNeedsLayout];
 }
 
 - (void)viewDidUnload
@@ -57,4 +94,63 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+// PjSwr8SS
+- (IBAction) calculate:(id)sender {
+    float voltValue, ohmValue, ampValue;
+    
+    [volts resignFirstResponder];
+    [ohms resignFirstResponder];
+    [amps resignFirstResponder];
+    
+    voltValue = [volts.text floatValue];
+    ohmValue = [ohms.text floatValue];
+    ampValue = [amps.text floatValue];
+    
+    @try {
+        if ([volts.text length] == 0) {
+            // E = I * R
+            voltValue = ohmValue * ampValue;
+            volts.text = [NSString stringWithFormat:@"%.3f V", voltValue];
+        } else if ([ohms.text length] == 0) {
+            // R = E / I
+            if (ampValue == 0.0) {
+                @throw [NSException exceptionWithName:@"Problem" reason:@"Amps can't be zero" userInfo:nil];
+            }
+            ohmValue = voltValue / ampValue;
+            ohms.text = [NSString stringWithFormat:@"%.3f Ω", ohmValue];
+        } else {    // calculate amps by default
+            // I = E / R
+            if (ohmValue == 0.0) {
+                @throw [NSException exceptionWithName:@"Problem" reason:@"Ohms can't be zero" userInfo:nil];
+            }
+            ampValue = voltValue / ohmValue;
+            amps.text = [NSString stringWithFormat:@"%.3f A", ampValue];
+        }
+    } @catch (NSException *ex) {
+        UIAlertView *av = [[UIAlertView alloc] initWithTitle:ex.name message:ex.reason delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+        [av show];
+        [av release];
+    }
+}
+
+- (IBAction)reset:(id)sender {
+    resetButton.hidden = YES;
+    volts.text = @"";
+    ohms.text = @"";
+    amps.text = @"";
+}
+
+//http://pastebin.com/fyibebQV
+- (IBAction) checkResetButton:(id)sender {
+    if ([ohms.text length] == 0 && [volts.text length] == 0 && [amps.text length] == 0) {
+        resetButton.hidden = YES;
+    } else {
+        resetButton.hidden = NO;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self calculate:textField];
+    return YES;
+}
 @end
